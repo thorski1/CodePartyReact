597d636b3af6c3941f67b74cfa337a0204085147
sthoyre@gmail.com <samuelthoyre@C02L94W0F6T6L.local>
2019-09-06 14:30:19 -0400

    end of Section 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -9,9 +9,20 @@ class EventForm extends Component {
 		venue: "",
 		hostedBy: ""
 	};
+	componentDidMount() {
+		if (this.props.selectedEvent !== null) {
+			this.setState({
+				...this.props.selectedEvent
+			});
+		}
+	}
 	handleFormSubmit = evt => {
 		evt.preventDefault();
-		this.props.createEvent(this.state);
+		if (this.state.id) {
+			this.props.updateEvent(this.state);
+		} else {
+			this.props.createEvent(this.state);
+		}
 	};
 	handleInputChange = ({ target: { name, value } }) => {
 		this.setState({
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -10,7 +10,7 @@ import EventListAttendee from "./EventListAttendee";
 
 class EventListItem extends Component {
 	render() {
-		const { event } = this.props;
+		const { event, selectEvent, deleteEvent } = this.props;
 		return (
 			<Segment.Group>
 				<Segment>
@@ -50,6 +50,14 @@ class EventListItem extends Component {
 				<Segment clearing>
 					<span>{event.description}</span>
 					<Button
+						onClick={() => deleteEvent(event.id)}
+						as="a"
+						color="red"
+						floated="right"
+						content="Delete"
+					/>
+					<Button
+						onClick={() => selectEvent(event)}
 						as="a"
 						color="teal"
 						floated="right"
